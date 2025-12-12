47eb9bc97bbf5c655052c0e6ff72d3c5dc8d0d2b
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-12 19:48:51 +0300

    Update cpu_temp.sh

--- scripts/cpu_temp.sh
+++ scripts/cpu_temp.sh
@@ -2,10 +2,12 @@
 
 mkdir -p "$HOME/cpu_directory"
 LOG_FILE="$HOME/cpu_directory/temperature.log"
+LOG_FILE_COLLECTOR="$HOME/cpu_directory/temperature.log"
 dosya="/sys/class/thermal/thermal_zone0/temp" 
 while true; do
 temp=$(cat "$dosya")
 temp=$(( $temp / 1000 ))
+echo "$temp" > "$LOG_FILE_COLLECTOR"
 if [ "$temp" -gt 80 ]
 then
 echo "CPU Temperature: $temp - $(date)" >> "$LOG_FILE"
