3c37fc1fa35ed4f671e69d5a3a7f1242ce5f960d
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-12 19:52:15 +0300

    Update cpu_termal.py

--- src/cpu_termal.py
+++ src/cpu_termal.py
@@ -2,11 +2,9 @@ import time
 import os
 home=os.path.expanduser("~")
 cpu_temperature_file = "/sys/class/thermal/thermal_zone0/temp"
-data_csv = f"{home}/cpu_directory/cpu_temperature.csv"
+data_csv = f"{home}/cpu_directory/collector.log"
 
 sayac = 0
-with open(data_csv,"w") as f:
-    f.write("Time,Temperature\n")
 
 while 1:
     with open(cpu_temperature_file,"r") as f:
@@ -16,3 +14,24 @@ while 1:
     with open(data_csv,"a") as f:
         f.write(f"{sayac},{temp}\n")
     time.sleep(10)
+
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+home=os.path.expanduser("~")
+data_csv = f"{home}/cpu_directory/collector.log"
+
+data = pd.read_csv(data_csv)
+
+time_list = data["Time"].tolist()
+temp_list = data["Temperature"].tolist()
+
+plt.figure(figsize=(6,6))
+plt.plot(range(len(time_list)),temp_list,color="red",linewidth=2)
+plt.title("CPU Temperature During Time")
+plt.xlabel("Time (s)")
+plt.ylabel("Temperature")
+plt.grid(True)
+plt.show()
