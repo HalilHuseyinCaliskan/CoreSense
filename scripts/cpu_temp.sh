83b8e3a9cddaa2492883493e40995a545c41b239
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-12 19:49:17 +0300

    Update cpu_temp.sh

--- scripts/cpu_temp.sh
+++ scripts/cpu_temp.sh
@@ -2,7 +2,7 @@
 
 mkdir -p "$HOME/cpu_directory"
 LOG_FILE="$HOME/cpu_directory/temperature.log"
-LOG_FILE_COLLECTOR="$HOME/cpu_directory/temperature.log"
+LOG_FILE_COLLECTOR="$HOME/cpu_directory/collector.log"
 dosya="/sys/class/thermal/thermal_zone0/temp" 
 while true; do
 temp=$(cat "$dosya")
