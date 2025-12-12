fe90e4e606ff2b58aa252b1d26f5daada7ecc251
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-12 20:05:08 +0300

    Update cpu_termal.py

--- src/cpu_termal.py
+++ src/cpu_termal.py
@@ -1,35 +1,18 @@
-import time
-import os
-home=os.path.expanduser("~")
-cpu_temperature_file = "/sys/class/thermal/thermal_zone0/temp"
-data_csv = f"{home}/cpu_directory/collector.log"
-
-sayac = 0
-
-while 1:
-    with open(cpu_temperature_file,"r") as f:
-        temp = float(f.read())
-        temp = temp / 1000
-        sayac = sayac+10
-    with open(data_csv,"a") as f:
-        f.write(f"{sayac},{temp}\n")
-    time.sleep(10)
-
-
-
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 home=os.path.expanduser("~")
-data_csv = f"{home}/cpu_directory/collector.log"
-
-data = pd.read_csv(data_csv)
-
-time_list = data["Time"].tolist()
-temp_list = data["Temperature"].tolist()
-
+data=f"{home}/cpu_directory/collector.log"
+temp_list=list()
+with open(data,"r") as f:
+    for line in f:
+        temp = float(line.strip())
+        temp_list.append(temp)
+        if len(temp_list)>=500:
+            temp_list.pop()
+    
 plt.figure(figsize=(6,6))
-plt.plot(range(len(time_list)),temp_list,color="red",linewidth=2)
+plt.plot(range(len(temp_list)),temp_list,color="red",linewidth=2)
 plt.title("CPU Temperature During Time")
 plt.xlabel("Time (s)")
 plt.ylabel("Temperature")
