f322ec0990e63559e7b0cb0a54919c346010be91
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-13 12:28:05 +0300

    Create cpu_cores_thermal.py

---
+++ src/cpu_cores_thermal.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import os
+home=os.path.expanduser("~")
+
+cpu_core_list=list()
+cpu_temperature_list=list()
+
+cpu_core_number_file = f"{home}/cpu_directory/cpu_cores.log"
+with open(cpu_core_number_file,"r",newline="") as f:
+    cpu_cores = int(f.read().strip())
+
+for i in range(cpu_cores)
+    dosya = f"/sys/class/thermal/thermal_zone{i}/temp"
+    cpu_core_list.append(f"cpu{i}")
+    with open(dosya,"r") as f:
+        cpu_temperature = int(f.read())
+        cpu_temperature_list.append(cpu_temperature)
+
+plt.bar(cpu_core_list,cpu_temperature_list,width=0.5,color="skyblue",edgecolor="black")
+for i,v in enumerate(cpu_temperature_list):
+    plt.text(i,v+0.5,str(v),ha="center")
+plt.title("CPU Cores and Temperatures")
+plt.xlabel("CPU Cores")
+plt.ylabel("CPU Temperatures")
+plt.show()
