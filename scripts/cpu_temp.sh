27fa9b9e70f82e694f62c12d429b5f151999a5d8
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-13 10:26:20 +0000

    Update cpu_temp.sh

--- scripts/cpu_temp.sh
+++ scripts/cpu_temp.sh
@@ -4,8 +4,8 @@ mkdir -p "$HOME/cpu_directory"
 LOG_FILE="$HOME/cpu_directory/temperature.log"
 LOG_FILE_COLLECTOR="$HOME/cpu_directory/collector.log"
 LOG_FILE_CPU_CORES="$HOME/cpu_directory/cpu_cores.log"
-dosya="/sys/class/thermal/thermal_zone0/temp" 
-number_cores=$(cat /proc/cpuinfo | grep "cpu cores" | awk '{print $4}')
+dosya="/sys/class/thermal/thermal_zone0/temp"
+number_cores=$(cat /proc/cpuinfo | grep "processor" | awk '{print $3}')
 echo "$number_cores" > "$LOG_FILE_CPU_CORES"
 while true; do
 temp=$(cat "$dosya")
