22bdec3b8587f0d5c5d5a2f7e259fb54c3539566
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-13 10:24:17 +0000

    Update cpu_cores_thermal.py

--- src/cpu_cores_thermal.py
+++ src/cpu_cores_thermal.py
@@ -4,16 +4,19 @@ home=os.path.expanduser("~")
 
 cpu_core_list=list()
 cpu_temperature_list=list()
-
+length=-1
 cpu_core_number_file = f"{home}/cpu_directory/cpu_cores.log"
 with open(cpu_core_number_file,"r",newline="") as f:
-    cpu_cores = int(f.read().strip())
+    for i in f:
+      if i!="":
+         print(length)
+         length=length+1
 
-for i in range(cpu_cores)
+for i in range(length):
     dosya = f"/sys/class/thermal/thermal_zone{i}/temp"
     cpu_core_list.append(f"cpu{i}")
     with open(dosya,"r") as f:
-        cpu_temperature = int(f.read())
+        cpu_temperature = int(f.read())/1000
         cpu_temperature_list.append(cpu_temperature)
 
 plt.bar(cpu_core_list,cpu_temperature_list,width=0.5,color="skyblue",edgecolor="black")
