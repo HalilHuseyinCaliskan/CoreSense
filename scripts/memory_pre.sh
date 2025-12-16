528240cfda7006e3098331af63fabeeb619705dc
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-16 15:00:32 +0300

    Update and rename memory_pre.sh to permissions_and_cli.sh

--- scripts/memory_pre.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x src/memory_pie.py
-sudo mv src/memory_pie.py /usr/local/bin/memory_pie
---
+++ scripts/permissions_and_cli.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+chmod +x $HOME/CoreSense/src/memory_pie.py
+sudo mv $HOME/CoreSense/src/memory_pie.py /usr/local/bin/memory_pie
+chmod +x $HOME/CoreSense/src/cpu_termal.py
+sudo mv $HOME/CoreSense/src/cpu_termal.py /usr/local/bin/cpu_thermal
+chmod +x $HOME/CoreSense/src/cpu_cores_thermal.py
+sudo mv $HOME/CoreSense/src/cpu_cores_thermal.py /usr/local/bin/cpu_cores_thermal
