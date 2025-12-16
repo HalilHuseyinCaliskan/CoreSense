1154f834ec3b75813334b4957485315d93eb9cdc
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-16 16:07:51 +0300

    Update permissions_and_cli.sh

--- scripts/permissions_and_cli.sh
+++ scripts/permissions_and_cli.sh
@@ -4,13 +4,18 @@ sudo mv $HOME/CoreSense/src/memory_pie.py /usr/local/bin/memory_pie
 chmod +x $HOME/CoreSense/src/cpu_termal.py
 sudo mv $HOME/CoreSense/src/cpu_termal.py /usr/local/bin/cpu_thermal
 chmod +x $HOME/CoreSense/src/cpu_cores_thermal.py
+chmod +x $HOME/CoreSense/src/lstm_anomali_predictor.py
 sudo mv $HOME/CoreSense/src/cpu_cores_thermal.py /usr/local/bin/cpu_cores_thermal
 chmod +x $HOME/CoreSense/scripts/shutdown.sh
 chmod +x $HOME/CoreSense/scripts/memory_usage.sh
 chmod +x $HOME/CoreSense/scripts/cpu_temp.sh
 chmod +x $HOME/CoreSense/scripts/commends.sh
 sudo mv $HOME/CoreSense/scripts/core_sense.service /etc/systemd/system/core_sense.service
+sudo mv $HOME/CoreSense/src/anomali_detection.service /etc/systemd/system/anomali_detection.service
 sudo systemctl daemon-reload
 sudo systemctl start core_sense.service
 sudo systemctl enable core_sense.service
 sudo systemctl restart core_sense.service
+sudo systemctl start anomali_detection.service
+sudo systemctl enable anomali_detection.service
+sudo systemctl restart anomali_detection.service
