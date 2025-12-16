5220605821d0e8cb6ff78ec7dcb4b0e8ad541b21
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-16 15:20:02 +0300

    Update permissions_and_cli.sh

--- scripts/permissions_and_cli.sh
+++ scripts/permissions_and_cli.sh
@@ -5,3 +5,7 @@ chmod +x $HOME/CoreSense/src/cpu_termal.py
 sudo mv $HOME/CoreSense/src/cpu_termal.py /usr/local/bin/cpu_thermal
 chmod +x $HOME/CoreSense/src/cpu_cores_thermal.py
 sudo mv $HOME/CoreSense/src/cpu_cores_thermal.py /usr/local/bin/cpu_cores_thermal
+chmod +x $HOME/CoreSense/scripts/shutdown.sh
+chmod +x $HOME/CoreSense/scripts/memory_usage.sh
+chmod +x $HOME/CoreSense/scripts/cpu_temp.sh
+chmod +x $HOME/CoreSense/scripts/commends.sh
