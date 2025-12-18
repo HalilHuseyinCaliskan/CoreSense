fbadd09cd4a7d1c1fa9a7900523064ab4de4a63a
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-18 18:29:25 +0300

    Update permissions_and_cli.sh

--- scripts/permissions_and_cli.sh
+++ scripts/permissions_and_cli.sh
@@ -14,8 +14,10 @@ chmod +x $HOME/CoreSense/scripts/shutdown.sh
 chmod +x $HOME/CoreSense/scripts/memory_usage.sh
 chmod +x $HOME/CoreSense/scripts/cpu_temp.sh
 chmod +x $HOME/CoreSense/scripts/commends.sh
+chmod +x $HOME/CoreSense/scripts/cpu_frequency.sh
 sudo mv $HOME/CoreSense/scripts/core_sense.service /etc/systemd/system/core_sense.service
 sudo mv $HOME/CoreSense/scripts/anomali_detection.service /etc/systemd/system/anomali_detection.service
+sudo mv $HOME/CoreSense/scripts/frequency.service /etc/systemd/system/frequency.service
 sudo systemctl daemon-reload
 sudo systemctl start core_sense.service
 sudo systemctl enable core_sense.service
@@ -23,3 +25,6 @@ sudo systemctl restart core_sense.service
 sudo systemctl start anomali_detection.service
 sudo systemctl enable anomali_detection.service
 sudo systemctl restart anomali_detection.service
+sudo systemctl start frequency.service
+sudo systemctl enable frequency.service
+sudo systemctl restart frequency.service
