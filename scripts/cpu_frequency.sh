6a854e5d09b70201595f0bb09d414cc9402a79d5
HalilHuseyinCaliskan <caliskanhalil815@gmail.com>
2025-12-18 18:36:34 +0300

    Update cpu_frequency.sh

--- scripts/cpu_frequency.sh
+++ scripts/cpu_frequency.sh
@@ -7,14 +7,16 @@ threshold_v1=50000
 cores=$((cores-1))
 for((i=0;i<cores;i++)); do
    echo userspace > "/sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor" 
+   echo "$freq_threshold" > "/sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq"
+   echo "Merhaba2"
 done
 echo "$cores"
 while true; do
 for ((i=0;i<cores;i++)); do
    temp=$(cat "/sys/class/thermal/thermal_zone$i/temp")
-   current_frequency=$(cat "/sys/devices/system/cpu/cpu$i/cpufreq/scaling_cur_freq")
+   current_frequency=$(cat "/sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq")
    echo "Merhaba2"
-   if [ "$temp" -gt "$threshold_v1" ];
+   if [ "$temp" -gt "$threshold_v1" ] && [ "$current_frequency" -gt "$freq_threshold" ];
    then
    formula=$((current_frequency*(100-5*(temp-threshold_v1)/threshold_v1)/100))
    echo "$formula" > "/sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq"
